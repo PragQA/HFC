f15b4366627ac7230df8ffa7893b6922c069b765
Pragna Bandi <63198723+PragQA@users.noreply.github.com>
2021-05-09 23:47:48 +0530

    Automation assignment

---
+++ AutomationTests/cypress/integration/UI/TC_AddToCart.spec.js
@@ -0,0 +1,16 @@
+describe('Add to cart functionality for Mamacita', function() {
+
+    it('Guest user flow', function() {
+        cy.clearCookies();
+        cy.visit('/')      
+        cy.get('.button-home').eq(0).scrollIntoView();
+        cy.get('.agree-cookie').contains('Ok').click();
+        cy.get('.button-home').eq(1).scrollIntoView();
+        cy.get('.button-home').eq(2).scrollIntoView().click({force:true});
+        cy.get('[id="address-input"]').type('Seidengasse 44, 1070 Wien, Austria',{ delay: 100 }).type('{enter}');
+        cy.wait(200);
+        cy.get('.image-link-container').eq(1).scrollIntoView().should('be.visible').click();
+        //Page is getting loaded after this, do not know the exact reason, need to know the flow of this and its related apis  and cookies if any
+        //Checkout page shows that the kitchen is not taking any orders, so it couldn't be tested
+    });
+    });
